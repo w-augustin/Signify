f636b453ba628cf86428891f99cc9fae3dba2c2e
Sharan Majumder <majumdersharan@gmail.com>
2025-03-31 02:02:08 -0400

    debug page functions, SM

--- app/src/test/java/com/android/signify/ExampleUnitTest.kt
+++ app/src/test/java/com/android/signify/ExampleUnitTest.kt
@@ -4,6 +4,8 @@ import org.junit.Test
 
 import org.junit.Assert.*
 
+import com.example.signifybasic.database.DBHelper
+
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
@@ -14,4 +16,7 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+
+
+
 }
