2887468899ce287107885309174e4f8fb4bd0cb5
Aidan Tambling <atambling@ufl.edu>
2025-04-13 20:30:17 -0400

    rolling back home fragment update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,7 +77,6 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation("com.google.code.gson:gson:2.10.1")
-    implementation("com.google.android:flexbox:3.0.0")
     implementation("androidx.preference:preference-ktx:1.2.1")
     implementation("com.github.bumptech.glide:glide:4.16.0")
     kapt("com.github.bumptech.glide:compiler:4.16.0")
